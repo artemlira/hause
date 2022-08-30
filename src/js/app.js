69c36a5cfc3e15594bc87fe5fa7ae616e587bf78
Lira Artem <artemlira84@gmail.com>
2022-08-30 21:11:59 +0300

    собрал шаблон, начал делать header

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+import * as flsFunctions from "./modules/functions.js";
+// import * as burgerMenu from "./modules/burger-menu.js";
+
+flsFunctions.isWebp();
+// burgerMenu.showMenu();
+
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+const swiper = new Swiper();
